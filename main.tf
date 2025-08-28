e79f2bc42b03bfdab330cc8916e4946481062958
Michael Wagner <m.wagner@cronos.de>
2025-08-28 10:00:17 +0200

    reingeknechtet

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~>4.14.0"
+    }
+  }
+}
+provider "azurerm" {
+  subscription_id = "9fae4087-6fd2-4b98-9ec6-8f869b9ff2b9"
+  resource_provider_registrations = "core"
+  features {}
+}
+resource "azurerm_resource_group" "dummy" {
+  name     = "rg-terraform-dummy"
+  location = "westeurope"
+}
